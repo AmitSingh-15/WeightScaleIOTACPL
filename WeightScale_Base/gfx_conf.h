dd5d8ff4161f971441fd2a6dc38114233df8f689
= <amitpsing.11@gmail.com>
2026-01-27 00:12:08 +0530

    fixed

--- WeightScale_Base/gfx_conf.h
+++ WeightScale_Base/gfx_conf.h
@@ -1,88 +1,126 @@
 #pragma once
 
+#include <lvgl.h>
+
 #define LGFX_USE_V1
 #include <LovyanGFX.hpp>
+#include <lgfx/v1/platforms/esp32s3/Panel_RGB.hpp>
+#include <lgfx/v1/platforms/esp32s3/Bus_RGB.hpp>
+#include <driver/i2c.h>
+
+/* ===============================
+   CrowPanel 7-inch configuration
+   =============================== */
+#define screenWidth   800
+#define screenHeight  480
 
 class LGFX : public lgfx::LGFX_Device
 {
-    lgfx::Panel_ILI9486 _panel_instance;
-    lgfx::Bus_SPI _bus_instance;
-    lgfx::Light_PWM _light_instance;
-    lgfx::Touch_XPT2046 _touch_instance;
-
 public:
+    lgfx::Bus_RGB     bus;
+    lgfx::Panel_RGB   panel;
+    lgfx::Light_PWM   backlight;
+    lgfx::Touch_GT911 touch;
+
     LGFX(void)
     {
+        /* ---------- Panel ---------- */
         {
-            auto cfg = _bus_instance.config();
-            cfg.spi_host = VSPI_HOST;
-            cfg.spi_mode = 0;
-            cfg.freq_write = 40000000;
-            cfg.freq_read = 16000000;
-            cfg.spi_3wire = false;
-            cfg.use_lock = true;
-            cfg.dma_channel = 1;
-            cfg.pin_sclk = 18;
-            cfg.pin_mosi = 23;
-            cfg.pin_miso = 19;
-            cfg.pin_dc = 2;
-            _bus_instance.config(cfg);
-            _panel_instance.setBus(&_bus_instance);
+            auto cfg = panel.config();
+            cfg.memory_width  = screenWidth;
+            cfg.memory_height = screenHeight;
+            cfg.panel_width   = screenWidth;
+            cfg.panel_height  = screenHeight;
+            cfg.offset_x      = 0;
+            cfg.offset_y      = 0;
+            panel.config(cfg);
         }
 
+        /* ---------- RGB Bus ---------- */
         {
-            auto cfg = _panel_instance.config();
-            cfg.pin_cs = 15;
-            cfg.pin_rst = -1;
-            cfg.pin_busy = -1;
-            cfg.memory_width = 800;
-            cfg.memory_height = 480;
-            cfg.panel_width = 800;
-            cfg.panel_height = 480;
-            cfg.offset_x = 0;
-            cfg.offset_y = 0;
-            cfg.offset_rotation = 0;
-            cfg.dummy_read_pixel = 8;
-            cfg.dummy_read_bits = 1;
-            cfg.readable = true;
-            cfg.invert = false;
-            cfg.rgb_order = false;
-            cfg.dlen_16bit = false;
-            cfg.bus_shared = true;
-            _panel_instance.config(cfg);
+            auto cfg = bus.config();
+            cfg.panel = &panel;
+
+            // Blue
+            cfg.pin_d0  = GPIO_NUM_15;
+            cfg.pin_d1  = GPIO_NUM_7;
+            cfg.pin_d2  = GPIO_NUM_6;
+            cfg.pin_d3  = GPIO_NUM_5;
+            cfg.pin_d4  = GPIO_NUM_4;
+
+            // Green
+            cfg.pin_d5  = GPIO_NUM_9;
+            cfg.pin_d6  = GPIO_NUM_46;
+            cfg.pin_d7  = GPIO_NUM_3;
+            cfg.pin_d8  = GPIO_NUM_8;
+            cfg.pin_d9  = GPIO_NUM_16;
+            cfg.pin_d10 = GPIO_NUM_1;
+
+            // Red
+            cfg.pin_d11 = GPIO_NUM_14;
+            cfg.pin_d12 = GPIO_NUM_21;
+            cfg.pin_d13 = GPIO_NUM_47;
+            cfg.pin_d14 = GPIO_NUM_48;
+            cfg.pin_d15 = GPIO_NUM_45;
+
+            cfg.pin_henable = GPIO_NUM_41;
+            cfg.pin_vsync   = GPIO_NUM_40;
+            cfg.pin_hsync   = GPIO_NUM_39;
+            cfg.pin_pclk    = GPIO_NUM_0;
+
+            cfg.freq_write = 12000000;
+
+            // Timing (CrowPanel official)
+            cfg.hsync_polarity    = 0;
+            cfg.hsync_front_porch = 40;
+            cfg.hsync_pulse_width = 48;
+            cfg.hsync_back_porch  = 40;
+
+            cfg.vsync_polarity    = 0;
+            cfg.vsync_front_porch = 1;
+            cfg.vsync_pulse_width = 31;
+            cfg.vsync_back_porch  = 13;
+
+            cfg.pclk_active_neg = 1;
+            cfg.de_idle_high    = 0;
+            cfg.pclk_idle_high  = 0;
+
+            bus.config(cfg);
+            panel.setBus(&bus);
         }
 
+        /* ---------- Backlight ---------- */
         {
-            auto cfg = _light_instance.config();
-            cfg.pin_bl = 21;
-            cfg.invert = false;
-            cfg.freq = 44100;
-            cfg.pwm_channel = 7;
-            _light_instance.config(cfg);
-            _panel_instance.setLight(&_light_instance);
+            auto cfg = backlight.config();
+            cfg.pin_bl = GPIO_NUM_2;
+            backlight.config(cfg);
+            panel.light(&backlight);
         }
 
+        /* ---------- Touch (GT911) ---------- */
         {
-            auto cfg = _touch_instance.config();
+            auto cfg = touch.config();
             cfg.x_min = 0;
-            cfg.x_max = 799;
+            cfg.x_max = screenWidth - 1;
             cfg.y_min = 0;
-            cfg.y_max = 479;
-            cfg.pin_int = 36;
+            cfg.y_max = screenHeight - 1;
+
+            cfg.pin_int = -1;
+            cfg.pin_rst = -1;
             cfg.bus_shared = true;
-            cfg.offset_rotation = 0;
-            cfg.i2c_port = 1;
-            cfg.i2c_addr = 0x38;
-            cfg.pin_sda = 33;
-            cfg.pin_scl = 32;
-            cfg.freq = 400000;
-            _touch_instance.config(cfg);
-            _panel_instance.setTouch(&_touch_instance);
+
+            cfg.i2c_port = I2C_NUM_1;
+            cfg.pin_sda  = GPIO_NUM_19;
+            cfg.pin_scl  = GPIO_NUM_20;
+            cfg.freq     = 400000;
+            cfg.i2c_addr = 0x14;
+
+            touch.config(cfg);
+            panel.setTouch(&touch);
         }
 
-        setPanel(&_panel_instance);
+        setPanel(&panel);
     }
 };
 
-#define screenWidth 800
-#define screenHeight 480
+extern LGFX tft;
